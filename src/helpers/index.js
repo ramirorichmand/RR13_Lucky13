d37046a410f1b38b67715d3229492eda5abe0faf
Ramiro <rrcolumbia21@gmail.com>
2023-06-21 07:25:05 +0100

    start

---
+++ src/helpers/index.js
@@ -0,0 +1,12 @@
+var rotateLeft = function (matrix) {
+  var rows = matrix.length;
+  var columns = matrix[0].length;
+  var res = [];
+  for (var row = 0; row < rows; ++row) {
+    res.push([]);
+    for (var column = 0; column < columns; ++column) {
+      res[row][column] = matrix[column][columns - row - 1];
+    }
+  }
+  return res;
+};
