2bf77277a3ccf72cef9e7929e582821af639965d
Ramiro <rrcolumbia21@gmail.com>
2023-06-22 07:21:30 +0100

    use board

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -4,3 +4,22 @@ import Cell from "./Cell";
 import { Board } from "../helper";
 import useEvent from "../hooks/useEvent";
 import GameOverlay from "./GameOverlay";
+
+const BoardView = () => {
+    const [board, setBoard] = useState(new Board());
+  
+    const handleKeyDown = (event) => {
+      if (board.hasWon()) {
+        return;
+      }
+  
+      if (event.keyCode >= 37 && event.keyCode <= 40) {
+        let direction = event.keyCode - 37;
+        let boardClone = Object.assign(
+          Object.create(Object.getPrototypeOf(board)),
+          board
+        );
+        let newBoard = boardClone.move(direction);
+        setBoard(newBoard);
+      }
+    };
